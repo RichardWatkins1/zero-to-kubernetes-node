ff61ab875e792fe9bb14e683d1ef11d142ba16f3
RichardWatkins <richardwatkins1@gmail.com>
2019-10-02 12:36:05 +0100

    up to rendering markdown to html

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const path = require('path')
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017'
+const multer = require('multer')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
 async function start() {
+  const db = await initMongo()
   app.set('view engine', 'pug')
   app.set('views', path.join(__dirname, 'views'))
   app.use(express.static(path.join(__dirname, 'public')))
@@ -14,7 +18,45 @@ async function start() {
     console.log(`App listening on http://localhost:${port}`)
   })
 
-  app.get('/', (req, res) => res.render('index'))
+  app.get('/', async (req, res) => {
+    res.render('index', { notes: await retrieveNotes(db) })
+  })
+
+  app.post(
+    '/note',
+    multer().none(),
+    async (req, res) => {
+      if (!req.body.upload && req.body.description) {
+        await saveNote(db, { description: req.body.description })
+        res.redirect('/')
+      }
+    }
+  )
+}
+
+async function initMongo() {
+  console.log('Initialising MongoDB...')
+  let success = false
+  while (!success) {
+    try {
+      client = await MongoClient.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+      success = true
+    } catch {
+      console.log('Error connecting to MongoDB, retrying in 1 second')
+      await new Promise(resolve => setTimeout(resolve, 1000))
+    }
+  }
+  console.log('MongoDB initialised')
+  return client.db('dev').collection('notes')
+}
+
+async function retrieveNotes(db) {
+  const notes = (await db.find().toArray()).reverse()
+  return notes
+}
+
+async function saveNote(db, note) {
+  await db.insertOne(note)
 }
 
 start()
